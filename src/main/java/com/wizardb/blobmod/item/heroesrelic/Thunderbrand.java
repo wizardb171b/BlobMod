01f4c597d3e483a087b6aff5e04c071d8a46560a
wizardb171b <benxuzhang@gmail.com>
2022-08-05 15:39:28 -0400

    added blutgang + beastfang, awakened eviserate and final awakened eviserate, modified atrocity, apocalypticflames, and ruinedsky, luin aoe added, soc ts cancel added, thunderrush lightning added

--- src/main/java/com/wizardb/blobmod/item/heroesrelic/Thunderbrand.java
+++ src/main/java/com/wizardb/blobmod/item/heroesrelic/Thunderbrand.java
@@ -2,7 +2,6 @@ package com.wizardb.blobmod.item.heroesrelic;
 
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
